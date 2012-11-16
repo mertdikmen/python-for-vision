a3cff4980f07356c0f6b189c69553fb19362f2f6
Mert Dikmen <mertdikmen@gmail.com>
2012-11-16 07:52:36 -0600

    face detection update

--- facedetect.py
+++ facedetect.py
@@ -1,9 +1,15 @@
 import cv2
-classifier = cv2.CascadeClassifier("/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml")
+classifier = cv2.CascadeClassifier("data/haarcascade_frontalface_alt.xml")
 camera = cv2.VideoCapture()
 camera.open(0)
+cv2.namedWindow('video')
 while True:
     frame = camera.read()[1]
     detections = classifier.detectMultiScale(frame)
-
-    print('n_faces: {}'.format(len(detections)))
+    for detection in detections:
+        cv2.rectangle(frame, tuple(detection[:2]), tuple(detection[:2] + detection[2:]), (255,0,0))
+    cv2.imshow('video',frame)
+    key = cv2.waitKey(1)
+    if key == 113:
+        break
+cv2.destroyAllWindows()
